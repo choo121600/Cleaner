5fefd117fda853d198469bd22049e765d756e5d5
Keith Choo <choo121600@gmail.com>
2022-01-02 03:43:44 +0900

    Connect(server): Connect MySQL and Secure private info

--- server/index.js
+++ server/index.js
@@ -1,5 +1,36 @@
 const express = require('express');
 const app = express();
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const dotenv = require('dotenv');
+
+dotenv.config({ path:"./.env" });
+
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+})
+
+connection.connect( (error) => {
+    if(error) {
+        console.log(error);
+    } else {
+        console.log("success") 
+    }
+});
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+
+app.post("/api/user/register", (req, res) => {
+    
+    return res.status(200)
+});
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
