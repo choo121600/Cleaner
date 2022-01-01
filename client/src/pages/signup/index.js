5c97f39b04001ae4f91c1045289b35ef73ccd904
Keith Choo <choo121600@gmail.com>
2022-01-02 07:33:27 +0900

    Feat(client&server): signin and login

---
+++ client/src/pages/signup/index.js
@@ -0,0 +1,100 @@
+import React, { Component } from 'react'
+import Axios from 'axios'
+
+export class signup extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            name: '',
+            password: '',
+            confirmPassword: '',
+            email: '',
+        }
+    }
+    render() {
+        const register = () => {
+            if(this.state.password !== this.state.confirmPassword) {
+                alert('Password and Confirm Password do not match')
+            } else {
+                Axios.post('http://localhost:5000/api/user/register', {
+                    name: this.state.name,
+                    email: this.state.email,
+                    password: this.state.password,
+                }).then(res => {
+                    console.log(res)
+                }).catch(err => {
+                    console.log(err)
+                })
+            }
+        }
+
+        const login = () => {
+            Axios.post('http://localhost:5000/api/user/login', {
+                email: this.state.email,
+                password: this.state.password,
+            }).then(res => {
+                console.log(res)
+            }).catch(err => {
+                console.log(err)
+            })
+        }
+
+        return (
+            <div className='SignUp'>
+                <div className='SignUp-container'>
+                    <div className='SignUp-title'>
+                        <h1>Sign Up</h1>
+                        <label>name</label>
+                        <input 
+                            type='text' 
+                            placeholder='Name' 
+                            onChange={(e) => this.setState({name: e.target.value})}
+                        />
+                        <label>Email</label>
+                        <input 
+                            type='email' 
+                            placeholder='Email'
+                            onChange={(e) => this.setState({email: e.target.value})}
+                        />
+                        <label>Password</label>
+                        <input 
+                            type='password' 
+                            placeholder='Password'
+                            onChange={(e) => this.setState({password: e.target.value})}
+                        />
+                        <label>Confirm Password</label>
+                        <input 
+                            type='password' 
+                            placeholder='Confirm Password' 
+                            onChange={(e) => this.setState({confirmPassword: e.target.value})}
+                        />
+                        <button
+                            onClick={register}
+                        >
+                            Sign Up
+                        </button>
+                    </div>
+
+                    <div className="login">
+                        <h1>Log In</h1>
+                        <label>Email</label>
+                        <input 
+                            type='email' 
+                            placeholder='Email'
+                            onChange={(e) => this.setState({email: e.target.value})}
+                        />
+                        <label>Password</label>
+                        <input 
+                            type='password' 
+                            placeholder='Password'
+                            onChange={(e) => this.setState({password: e.target.value})}
+                        />
+                        <button onClick={login}>Log In</button>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default signup
--- server/index.js
+++ server/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
 const mysql = require('mysql');
+const cors = require('cors');
+
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const dotenv = require('dotenv');
 
 dotenv.config({ path:"./.env" });
+
+app.use(cors());
 
 const connection = mysql.createConnection({
     host: process.env.DB_HOST,
@@ -27,9 +31,48 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 
 
+// 회원가입
 app.post("/api/user/register", (req, res) => {
-    
-    return res.status(200)
+    connection.query(`INSERT INTO users (name, email, password, createdAt) VALUES(?, ?, ?, now())`, 
+    [req.body.name, req.body.email, req.body.password], 
+    (error, results, fields) => { 
+        if(error) {
+            console.log(error);
+            res.send({
+                status: "error",
+                
+            })
+        } else {
+            res.send({
+                status: "success",
+            })
+        }
+    });
+});
+
+// 로그인
+app.post("/api/user/login", (req, res) => {
+    connection.query(`SELECT * FROM users WHERE email = ? AND password = ?`,
+    [req.body.email, req.body.password],
+    (error, results, fields) => {
+        if(error) {
+            console.log(error);
+            res.send({
+                status: "error",
+            })
+        } else {
+            if(results.length > 0) {
+                res.send({
+                    status: "success",
+                    data: results[0]
+                })
+            } else {
+                res.send({
+                    status: "error",
+                })
+            }
+        }
+    });
 });
 
 app.get('/', (req, res) => {
