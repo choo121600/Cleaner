15edba4a1287f76f51dbdb06b7997a5c46176f48
Keith Choo <choo121600@gmail.com>
2022-01-18 19:03:28 +0900

    Style(client): styling sign up form

---
+++ client/src/pages/signup/elements.js
@@ -0,0 +1,167 @@
+import styled from 'styled-components';
+import { NavLink as Link } from 'react-router-dom';
+
+
+export const Container = styled.div`
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    position: relative;
+    max-width: 1280px;
+    padding: 0 20px;
+    margin: 0 auto;
+    height: 100vh;
+    @media (max-width: 768px) {
+        padding: 0 20px;
+        height: auto;
+    }
+`;
+
+export const AppHeader = styled.header`
+    display: flex;
+    position: relative;
+    margin-bottom: 30px;
+    width: 100%;
+`;
+
+export const NavLink = styled(Link)`
+    color: #000;
+    font-size: 14px;
+    font-weight: 600;
+    text-decoration: none;
+    padding: 0 20px;
+    &:hover {
+        color: #000;
+    }
+    h1 {
+        color: #2828b3;
+    }
+`;
+
+export const SignUp = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    max-width: 488px;
+    width: 100%;
+`
+
+export const SignUpTitle = styled.div`
+    padding-bottom: 30px;
+    h1 {
+        font-family: 'Nanum Gothic', sans-serif;
+        font-weight: 400;
+    }
+`
+
+export const SignUpContainer = styled.div`
+    padding: 24px;
+    border: 1px solid #e6e6e6;
+    border-radius: 4px;
+`
+
+export const SignInLink = styled.div`
+    display: flex;
+    justify-content: flex-end;
+    margin-bottom: 30px;
+    h6 {
+        margin: 0;
+        font-size: 12px;
+        font-weight: 400;
+    }
+    a {
+        margin-left: 5px;
+        text-decoration: none;
+        &:hover {
+            color: #2828b3;
+            text-decoration: underline;
+        }
+        h6 {
+            color: #2828b3;
+            font-size: 12px;
+            font-weight: 600;
+            font-family: "san-serif";
+        }
+    }
+`
+
+export const SignUpForm = styled.form`
+    display: flex;
+    flex-direction: column;
+`
+
+export const NameFormGroup = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    column-gap: 8px;
+`
+
+export const GroupContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 20px;
+    column-gap: 10px;
+    input {
+        width: 100%;
+        display: block;
+        padding: 0 16px;
+        border: 1px solid #e6e6e6;
+        border-radius: 4px;
+        height: 40px;
+    }
+`
+
+export const FormContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 20px;
+`
+
+export const InputBox = styled.div`
+    display: flex;
+    align-items: center;
+    position: relative;
+    input {
+        width: 100%;
+        position: relative;
+        display: block;
+        padding: 0 16px;
+        border: 1px solid #e6e6e6;
+        border-radius: 4px;
+        height: 40px;
+    }
+`
+
+export const Agreement = styled.div`
+    display: flex;
+    margin-top: 10px;
+    align-items: center;
+    input {
+        width: 18px;
+        height: 18px;
+    }
+    label {
+        margin-left: 5px;
+        font-size: 12px;
+    }
+`
+
+export const Button = styled.button`
+    color: #fff;
+    font-size: 15px;
+    font-weight: 600;
+    font-family: "san-serif";
+    width: 100%;
+    display: block;
+    padding: 0 16px;
+    margin-top: 20px;
+    background-color: #2828b3;
+    border: 1px solid transparent;
+    border-radius: 4px;
+    min-width: 132px;
+    height: 48px;
+    cursor: pointer;
+    &:hover {
+        background-color: #072A6C;
+    }
+`
--- client/src/pages/signup/index.js
+++ client/src/pages/signup/index.js
@@ -1,30 +1,35 @@
-import React, { Component, useEffect } from 'react'
+import React, { Component } from 'react'
 import Axios from 'axios'
+
+import { 
+    AppHeader, 
+    Container, 
+    NavLink, 
+    SignInLink, 
+    SignUp, 
+    SignUpContainer,
+    SignUpTitle, 
+    SignUpForm,
+    NameFormGroup,
+    GroupContainer,
+    FormContainer,
+    InputBox,
+    Button,
+    Agreement,
+} from "./elements";
+
 
 export class signup extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            name: '',
-            password: '',
+            brand: 'cleaner',
+            firstName: '',
+            lastName: '',
             confirmPassword: '',
-            email: '',
             emailReg: '',
             passwordReg: '',
-            loginStatus: false,
         }
-    }
-    componentDidMount = () => {
-        Axios.get('http://localhost:5000/api/user/login').then(res => {
-            if (res.data.loggedIn) {
-                console.log(res)
-                this.setState({
-                    loginStatus: true,
-                })
-            }
-        }).catch(err => {
-            console.log(err)
-        })
     }
 
     render() {
@@ -34,7 +39,8 @@ export class signup extends Component {
                 alert('Password and Confirm Password do not match')
             } else {
                 Axios.post('http://localhost:5000/api/user/register', {
-                    name: this.state.name,
+                    firstName: this.state.firstName,
+                    lastName: this.state.lastName,
                     email: this.state.emailReg,
                     password: this.state.passwordReg,
                 }).then(res => {
@@ -45,71 +51,82 @@ export class signup extends Component {
             }
         }
 
-        const login = () => {
-            Axios.post('http://localhost:5000/api/user/login', {
-                email: this.state.email,
-                password: this.state.password,
-            }).then(res => {
-                console.log(res)
-            }).catch(err => {
-                console.log(err)
-            })
-        }
+
 
         return (
-            <div className='SignUp'>
-                <div className='SignUp-container'>
-                    <div className='SignUp-title'>
-                        <h1>Sign Up</h1>
-                        <label>name</label>
-                        <input 
-                            type='text' 
-                            placeholder='Name' 
-                            onChange={(e) => this.setState({name: e.target.value})}
-                        />
-                        <label>Email</label>
-                        <input 
-                            type='email' 
-                            placeholder='Email'
-                            onChange={(e) => this.setState({emailReg: e.target.value})}
-                        />
-                        <label>Password</label>
-                        <input 
-                            type='password' 
-                            placeholder='Password'
-                            onChange={(e) => this.setState({passwordReg: e.target.value})}
-                        />
-                        <label>Confirm Password</label>
-                        <input 
-                            type='password' 
-                            placeholder='Confirm Password' 
-                            onChange={(e) => this.setState({confirmPassword: e.target.value})}
-                        />
-                        <button
-                            onClick={register}
-                        >
-                            Sign Up
-                        </button>
-                    </div>
-
-                    <div className="login">
-                        <h1>Log In</h1>
-                        <label>Email</label>
-                        <input 
-                            type='email' 
-                            placeholder='Email'
-                            onChange={(e) => this.setState({email: e.target.value})}
-                        />
-                        <label>Password</label>
-                        <input 
-                            type='password' 
-                            placeholder='Password'
-                            onChange={(e) => this.setState({password: e.target.value})}
-                        />
-                        <button onClick={login}>Log In</button>
-                    </div>
-                </div>
-            </div>
+            <Container>
+                <AppHeader>
+                    <NavLink to="/">
+                        <h1>{this.state.brand}</h1>
+                    </NavLink>
+                </AppHeader>
+                <SignUp>
+                    <SignUpTitle>
+                        <h1>회원 가입</h1>
+                        <p>Create your account</p>
+                    </SignUpTitle>
+                    <SignUpContainer>
+                        <SignInLink>
+                            <h6>이미 회원이신가요?</h6>
+                            <a href='/login'><h6>Sign in</h6></a>
+                        </SignInLink>
+                        <SignUpForm>
+                            <NameFormGroup>
+                                <GroupContainer>
+                                    <label htmlFor="firstName">First Name</label>
+                                    <InputBox>
+                                        <input type="text" id="firstName" onChange={(e) => this.setState({firstName: e.target.value})}/>
+                                    </InputBox>
+                                </GroupContainer>
+                                <GroupContainer>
+                                    <label htmlFor="lastName">Last Name</label>
+                                    <InputBox>
+                                        <input type="text" id="lastName" onChange={(e) => this.setState({lastName: e.target.value})}/>
+                                    </InputBox>
+                                </GroupContainer>
+                            </NameFormGroup>
+                            <FormContainer>
+                                <label>Email</label>
+                                <InputBox>
+                                    <input 
+                                        type='email' 
+                                        onChange={(e) => this.setState({emailReg: e.target.value})}
+                                    />
+                                </InputBox>
+                            </FormContainer>
+                            <FormContainer>
+                                <label>Password</label>
+                                <InputBox>
+                                    <input 
+                                        type='password' 
+                                        onChange={(e) => this.setState({passwordReg: e.target.value})}
+                                    />
+                                </InputBox>
+                            </FormContainer>
+                            <FormContainer>
+                                <label>Confirm Password</label>
+                                <InputBox>
+                                    <input 
+                                        type='password' 
+                                        onChange={(e) => this.setState({confirmPassword: e.target.value})}
+                                    />
+                                </InputBox>
+                            </FormContainer>
+                            <Agreement>
+                                <input type="checkbox" id="agree" />
+                                <label>
+                                    이 서비스를 이용하는 것에 동의합니다.
+                                </label>
+                            </Agreement>
+                            <Button
+                                onClick={register}
+                            >
+                                Create Account
+                            </Button>
+                        </SignUpForm>
+                    </SignUpContainer>
+                </SignUp>
+            </Container>
         )
     }
 }
