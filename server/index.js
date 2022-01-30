b0fc767ee950cb1922fcf04c23fcf8d5a7e82bfe
Keith Choo <choo121600@gmail.com>
2022-01-30 17:22:44 +0900

    Feat(client&server): Set Proxy and Concurrently

--- server/index.js
+++ server/index.js
@@ -134,6 +134,8 @@ app.get('/', (req, res) => {
 });
 
 
+const port = process.env.PORT || 5000;
 
-
-app.listen(5000);
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
